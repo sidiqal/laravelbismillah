7a1b339bd9acd00121aa9c271f0847b78ff63558
sidiqal <sidiqtamvan@gmail.com>
2021-02-25 03:16:18 +0000

    coba routing

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/user', function () {
+    $user = "Halo bro apa kabar";
+    return $user;
 });
